5df7e470f14c4ba5d4ea268e3215f1cdbde5119d
sakshi1924 <82995567+sakshi1924@users.noreply.github.com>
2021-04-25 19:00:52 +0530

    Add files via upload

---
+++ main-6.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+#include <stdio.h>
+ 
+int sum (int a);
+ 
+int main()
+{
+    int num, result;
+ 
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    result = sum(num);
+    printf("Sum of digits in %d is %d\n", num, result);
+    return 0;
+}
+ 
+int sum (int num)
+{
+    if (num != 0)
+    {
+        return (num % 10 + sum (num / 10));
+    }
+    else
+    {
+       return 0;
+    }
+}
